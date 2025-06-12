475b6a003fbb516c8c84240a63adc7d61564ca84
reyesharold <reyesharold007@gmail.com>
2025-06-12 09:42:13 +0800

    added Entities and DbContext

--- ApiFirstProj/Program.cs
+++ ApiFirstProj/Program.cs
@@ -1,8 +1,15 @@
+using ApiFirstProj.AppDbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>( options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
