a7a581bc23ae6c7bf5eae03792317c7f20836d28
reyesharold <reyesharold007@gmail.com>
2025-06-16 04:58:15 +0800

    added Swagger and Versioning

---
+++ ApiFirstProj/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,45 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
+using System.Diagnostics;
+
+namespace ApiFirstProj.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        public static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo
+            {
+                Title = "Example Web API",
+                Version = description.ApiVersion.ToString(),
+                Description = "Sample Description",
+                Contact = new OpenApiContact { Name = "Sample Contact Name", Email = "Sample Contact EMail"},
+                License = new OpenApiLicense { Name =  "Sample License Name", Url = new Uri("https://opensource.org/licenses/MIT") }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += "This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
